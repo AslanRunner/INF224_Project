de2f262fc993eb6135df9327f2ff9d6e547311f3
Aslan Yusuf Zeybek <aslanyusuf05@outlook.com>
2025-12-12 14:54:02 +0300

    Update graph.h

--- graph.h
+++ graph.h
@@ -7,48 +7,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 typedef struct {
     int matrix[NUM_NODES][NUM_NODES];
     int degree[NUM_NODES];
     int num_edges;
 } Graph;
 
-// Topluluk bilgisini tutmak için basit yapı
 typedef struct {
-    int* node_to_comm;      // Her düğümün hangi toplulukta olduğu (0-33 arası ID)
-    int num_community;    // Şu an kaç topluluk var?
+    int* node_to_comm; // community (0-33 IDs)
+    int num_community;   
 } Communities;
 
-// Ilk halini baslat (her sey 0 degerli)
 void initGraph(Graph* graph);
-
-// Baglanti Ekleme Fonksiyonu (Undirected - Yonsuz)
 void addEdge(Graph* graph, int src, int dest);
-
-// Dosyadan baglantilari oku (78 baglanti olmasi lazim)
 void loadFromFile(Graph* graph, char* filename);
-
-// Matrisi Ekrana Yazdir (Kontrol icin)
 void printMatrix(Graph* graph); 
-
-// Topluluk sayisini guncellemek icin
 void update_community_count(Communities* communs);
-
-// Toplulugun boyutunu ogrenmek icin
 int get_comm_size(Communities* communs, int comm);
-
-// Final modularity'i hesapla
 double CalculateModularity(Graph* graph, int* partition);
-
-// Topluluklari bastirmak icin
 void print_communities(int *commties, int max_id, char *title);
-
 void runFastGreedy(Graph *graph);
-
 void runLouvain(Graph *graph);
-
 void runMyAlgorithm(Graph *graph);
-
 
 #endif
